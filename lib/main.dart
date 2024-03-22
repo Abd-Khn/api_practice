38f90aa42fdd5d77cc235e606e3cd79602e09afe
Abdullah <ak_fury7@yahoo.com>
2024-03-22 11:23:06 +0500

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var client = http.Client();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("APIs Practice"),
+      ),
+      body: Container(
+        child: Text(
+          "Data",
+          style: TextStyle(fontWeight: FontWeight.w400, fontSize: 20),
+        ),
+        padding: EdgeInsets.all(20),
+      ),
+    );
+  }
+}
